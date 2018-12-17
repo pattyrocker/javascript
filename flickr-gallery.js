7be1bf49cf907d4009f75d59ac1789f2df58d41e
pattyrocker <layoutsdapatty@gmail.com>
2018-12-17 05:31:28 -0200

    Update flickr-gallery.js

--- flickr-gallery.js
+++ flickr-gallery.js
@@ -1,8 +1,9 @@
+
 function flickr(user){
 	$.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?id='+user+'&format=json&jsoncallback=?', function(data){
 		for(i=0; i<12; i++){
 			var image = data.items[i].media.m.replace(/(_m.jpg)/g, '_z.jpg');
-			$("<img />").attr("src", data.items[i].media.m).appendTo("#flickr").wrap("<a href='" + image + "' title='" + data.items[i].title + "' rel='shadowbox'><\/a>");
+			$("<img />").attr("src", data.items[i].media.m).appendTo("#flickr").wrap("<a href='" + image + "' title='" + data.items[i].title + "' rel='shadowbox[gallery]' class='item'><\/a>");
 		}
 		Shadowbox.init();
 	});
