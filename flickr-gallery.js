4fc3a2242c75a88be18673be8623751612fcebe1
pattyrocker <layoutsdapatty@gmail.com>
2018-12-16 23:31:06 -0200

    Add files via upload

---
+++ flickr-gallery.js
@@ -0,0 +1,13 @@
+function flickr(user){
+	$.getJSON('http://api.flickr.com/services/feeds/photos_public.gne?id='+user+'&format=json&jsoncallback=?', function(data){
+		for(i=0; i<12; i++){
+			var image = data.items[i].media.m.replace(/(_m.jpg)/g, '_z.jpg');
+			$("<img />").attr("src", data.items[i].media.m).appendTo("#flickr").wrap("<a href='" + image + "' title='" + data.items[i].title + "' rel='shadowbox'><\/a>");
+		}
+		Shadowbox.init();
+	});
+}
+
+$(document).ready(function(){
+	flickr('95607642@N04');
+});
