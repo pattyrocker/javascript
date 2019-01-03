9d68e66437eaedc74b0ad4e2a88b02cc4affc2b7
pattyrocker <layoutsdapatty@gmail.com>
2019-01-03 18:04:49 -0200

    Add files via upload

--- paginacaonumerada.js
+++ paginacaonumerada.js
@@ -77,10 +77,10 @@ pageNavi.show=function(f,e,a){
 		}
 	}
 	if(e<a){
-		b+='<a href="'+f[e+1]+'">'+pageNaviConf.nextText+"</a>"
+		b+='<a href="'+f[e+1]+'"><span class="nextpage">'+pageNaviConf.nextText+"</span></a>"
 	}
 	if(endPage<a){
-		b+='<a href="'+f[a]+'">'+pageNaviConf.lastText+"</a>"
+		b+='<a href="'+f[a]+'"><span class="prevpage">'+pageNaviConf.lastText+"</span></a>"
 	}
 	
 	document.write(b)
