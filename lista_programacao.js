6be5b69d64fd45bfce0f76871eedd478686fe0f8
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 17:46:05 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,5 +1,5 @@
-var calenData = [
-	{mes: "Agosto", dia_n: "26", dia_s: "Segunda",
+var calenData = 
+	{
      programacao: {
 	 	titulo: "TESTE: Credenciamento",
 		local: "Saguão C3",
@@ -7,42 +7,5 @@ var calenData = [
 		horario: "08:30",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 	}
-},
-	{mes: "Agosto",	dia_n: "27", dia_s: "Terça",
-     programacao: {
-	 	titulo: "LALALA",
-		local: "Saguão C3",
-		palestrante: "Nome do Palestrante",
-		horario: "08:30",
-		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	{mes: "Agosto",	dia_n: "28", dia_s: "Quarta",
-     programacao: {
-	 	titulo: "Testando",
-		local: "Saguão C3",
-		palestrante: "Nome do Palestrante",
-		horario: "08:30",
-		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	
-	{mes: "Agosto",	dia_n: "29", dia_s: "Quinta",
-     programacao: {
-	 	titulo: "Credenciamento",
-		local: "Saguão C3",
-		palestrante: "Nome do Palestrante",
-		horario: "08:30",
-		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	{mes: "Agosto",	dia_n: "30", dia_s: "Sexta",
-     programacao: {
-	 	titulo: "Credenciamento",
-		local: "Saguão C3",
-		palestrante: "Nome do Palestrante",
-		horario: "08:30",
-		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-}	
-]
+   }	
+
