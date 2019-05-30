575ec08ebc6c257b18f344708cf364c112801c56
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 17:51:36 -0300

    Update and rename lista_programacao.json to lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,4 +1,4 @@
-{
+[
      programacao: {
 	 	titulo: "TESTE: Credenciamento",
 		local: "Saguão C3",
@@ -6,5 +6,5 @@
 		horario: "08:30",
 		descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 		}
-   }	
+]	
 
