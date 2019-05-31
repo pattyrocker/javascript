60dbf21937d4ddf05c34a71ff1a1018771e4202d
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 22:31:07 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -2,27 +2,18 @@
   {
 
     "id": 1,
-	"local": "Saguão C3",
-	"palestrante": "Nome do Palestrante 1",
-	"horario": "08:30",
     "titulo": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     "descricao": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
   },
   {
 
     "id": 2,
-	"local": "Saguão C3",
-	"palestrante": "Nome do Palestrante 2",
-	"horario": "08:30",
     "titulo": "qui est esse",
     "descricao": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
   },
   {
 
     "id": 3,
- 	"local": "Saguão C3",
-	"palestrante": "Nome do Palestrante 3",
-	"horario": "08:30",
     "titulo": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
     "descricao": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
   },
@@ -30,9 +21,6 @@
 
     "id": 4,
     "titulo": "eum et est occaecati",
-	"local": "Saguão C3",
-	"palestrante": "Nome do Palestrante 4",
-	"horario": "08:30",	
     "descricao": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
   }
 ]
