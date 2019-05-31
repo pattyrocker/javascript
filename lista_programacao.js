454d411cc74cd98a7d44ebd6103be8759310e9aa
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 23:53:29 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -7,14 +7,7 @@
 		"palestrante": "Nome do Palestrante",
 		"horario": "08:30",
 		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
- 	      },
-	      { 
-		"titulo": "didididid",
-		"local": "Saguão C3",
-		"palestrante": "Nome do Palestrante",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
  	      }
-	      
+ 
   } 
 ]
