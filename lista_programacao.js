c62cd374000e1713b265b0576afead1be4b99c3e
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 22:32:44 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -2,25 +2,25 @@
   {
 
     "id": 1,
-    "titulo": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
     "descricao": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
   },
   {
 
     "id": 2,
-    "titulo": "qui est esse",
+    "title": "qui est esse",
     "descricao": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
   },
   {
 
     "id": 3,
-    "titulo": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
     "descricao": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
   },
   {
 
     "id": 4,
-    "titulo": "eum et est occaecati",
+    "title": "eum et est occaecati",
     "descricao": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
   }
 ]
