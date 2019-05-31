d85d85254e525b04522df9040e7e458ef275f202
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 22:23:52 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,50 +1,27 @@
 [
-	{"mes": "Agosto", 
-	"dia_n": "26",
-	"dia_s": "Segunda",
-    "programacao": {
-	 	"titulo": "Credenciamento",
-		"local": "Saguão C3",
-		"palestrante": "Nome do "palestrante"",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	{"mes": "Agosto",	"dia_n": "27", "dia_s": "Terça",
-     "programacao": {
-	 	"titulo": "Credenciamento",
-		"local": "Saguão C3",
-		"palestrante": "Nome do "palestrante"",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	{"mes": "Agosto",	"dia_n": "28", "dia_s": "Quarta",
-     "programacao": {
-	 	"titulo": "Credenciamento",
-		"local": "Saguão C3",
-		"palestrante": "Nome do "palestrante"",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	
-	{"mes": "Agosto",	"dia_n": "29", "dia_s": "Quinta",
-     "programacao": {
-	 	"titulo": "Credenciamento",
-		"local": "Saguão C3",
-		"palestrante": "Nome do "palestrante"",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-},
-	{"mes": "Agosto",	"dia_n": "30", "dia_s": "Sexta",
-     "programacao": {
-	 	"titulo": "Credenciamento",
-		"local": "Saguão C3",
-		"palestrante": "Nome do "palestrante"",
-		"horario": "08:30",
-		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
-	}
-}	
+  {
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  }
 ]
+	
