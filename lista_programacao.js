0ad085cdae0f4d44e4b97a8e952c4167f44d6552
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 23:05:04 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,26 +1,5 @@
-[
-  {
-    "testo": [{
-     "dia": "111",
-	  ]},
-	  
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-  },
-  {
-    "userId": 2,
-    "id": 100,
-    "title": "at nam consequatur ea labore ea harum",
-    "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
-  }
-]
-    {
-    "userId": 10,
-    "id": 100,
-    "title": "at nam consequatur ea labore ea harum",
-	
-    "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
-  }
-]
+{
+"name":"John",
+"age":30,
+"cars":[ "Ford", "BMW", "Fiat" ]
+} 
