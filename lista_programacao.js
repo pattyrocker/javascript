3ff75d6405a05a81183c3d57d848408f8690966a
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 23:41:05 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -6,6 +6,13 @@
 		"palestrante": "Nome do Palestrante",
 		"horario": "08:30",
 		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
+		},
+	        { 
+		"titulo": "Credenciamento 2",
+		"local": "Saguão C3 2",
+		"palestrante": "Nome do Palestrante 2",
+		"horario": "08:30",
+		"descricao": " 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 		}
   } 
 ]
