87b83ab452665253e788e4a9c28672cd877aeccc
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 22:48:52 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,8 +1,9 @@
 [
   {
-    "teste": [
-      "dia": "26",
-     ],
+    "testo": [{
+     "dia": "111",
+	  ]},
+	  
     "userId": 1,
     "id": 1,
     "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
