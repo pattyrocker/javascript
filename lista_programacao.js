6f40d350d19ea68bdc7cbc1c8524de080ba759f3
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 22:20:03 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,6 +1,8 @@
 [
-	{mes: "Agosto", dia_n: "26", dia_s: "Segunda",
-     programacao: {
+	{"mes": "Agosto", 
+	"dia_n": "26",
+	"dia_s": "Segunda",
+    "programacao": {
 	 	"titulo": "Credenciamento",
 		"local": "Saguão C3",
 		"palestrante": "Nome do "palestrante"",
@@ -8,8 +10,8 @@
 		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 	}
 },
-	{mes: "Agosto",	dia_n: "27", dia_s: "Terça",
-     programacao: {
+	{"mes": "Agosto",	"dia_n": "27", "dia_s": "Terça",
+     "programacao": {
 	 	"titulo": "Credenciamento",
 		"local": "Saguão C3",
 		"palestrante": "Nome do "palestrante"",
@@ -17,8 +19,8 @@
 		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 	}
 },
-	{mes: "Agosto",	dia_n: "28", dia_s: "Quarta",
-     programacao: {
+	{"mes": "Agosto",	"dia_n": "28", "dia_s": "Quarta",
+     "programacao": {
 	 	"titulo": "Credenciamento",
 		"local": "Saguão C3",
 		"palestrante": "Nome do "palestrante"",
@@ -27,8 +29,8 @@
 	}
 },
 	
-	{mes: "Agosto",	dia_n: "29", dia_s: "Quinta",
-     programacao: {
+	{"mes": "Agosto",	"dia_n": "29", "dia_s": "Quinta",
+     "programacao": {
 	 	"titulo": "Credenciamento",
 		"local": "Saguão C3",
 		"palestrante": "Nome do "palestrante"",
@@ -36,8 +38,8 @@
 		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
 	}
 },
-	{mes: "Agosto",	dia_n: "30", dia_s: "Sexta",
-     programacao: {
+	{"mes": "Agosto",	"dia_n": "30", "dia_s": "Sexta",
+     "programacao": {
 	 	"titulo": "Credenciamento",
 		"local": "Saguão C3",
 		"palestrante": "Nome do "palestrante"",
