1e82d8931eb9d7d96d04640934b8ddd645067448
pattyrocker <layoutsdapatty@gmail.com>
2019-05-30 23:24:32 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,7 +1,13 @@
 [
   {
-"name":"John",
-"age":30,
-"cars":{ "modelo": "Fiat" }
+  	"titulo":"Credenciamento",
+	"local": "Saguão C3",
+	"programacao":{ 
+		"titulo": "Credenciamento",
+		"local": "Saguão C3",
+		"palestrante": "Nome do Palestrante",
+		"horario": "08:30",
+		"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ac viverra justo, a euismod felis. Duis ultricies convallis lacus vitae feugiat. In velit dolor, ornare at aliquet et, dictum euismod tortor. Nullam eros tellus, consequat a nisl nec, faucibus luctus massa. Cras sed iaculis tortor, et finibus nisl. Nulla eget leo id purus rhoncus efficitur auctor non erat. Pellentesque ut mi eget tortor pharetra mattis. Proin fringilla libero eget tellus commodo pharetra"
+		}
   } 
 ]
