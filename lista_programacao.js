82faea36136f6975eaa3292c510fd0972727e9bd
pattyrocker <layoutsdapatty@gmail.com>
2019-06-01 16:51:25 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,58 +1,12 @@
-[{
-		"dia_s": "segunda",
-		"dia_m": "26",
-		"mes": "agosto",
-		"programacao": [{
-				"titulo": "Palestra 1",
-				"horario": "8:30",
-				"local": "Saguão C3",
-				"Palestrante": "Nome do Palestrante",
-				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-			},
-			{
-				"titulo": "Palestra 2",
-				"horario": "10:30",
-				"local": "Lab C3",
-				"Palestrante": "Nome do Palestrante",
-				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-			}
-		]
-
-	},
-	{
-		"dia_s": "terça",
-		"dia_m": "27",
-		"mes": "agosto",
-		"programacao": {
-			"titulo": "Credenciamento",
-			"horario": "8:30",
-			"local": "Saguão C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
-	},
-	{
-		"dia_s": "quarta",
-		"dia_m": "28",
-		"mes": "agosto",
-		"programacao": {
-			"titulo": "Cerimônia de Abertura",
-			"horario": "8:30",
-			"local": "Auditório C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
-	},
-	{
-		"dia_s": "quinta",
-		"dia_m": "29",
-		"mes": "agosto",
-		"programacao": {
-			"titulo": "Cerimônia de Abertura",
-			"horario": "8:30",
-			"local": "Auditório C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
-	}
-]
+ "cars": {
+        "Nissan": [
+            {"model":"Sentra", "doors":4},
+            {"model":"Maxima", "doors":4},
+            {"model":"Skyline", "doors":2}
+        ],
+        "Ford": [
+            {"model":"Taurus", "doors":4},
+            {"model":"Escort", "doors":4}
+        ]
+    }
+}
