8b823b031a8ffcbbeed341e659d31cc58fd08a57
pattyrocker <layoutsdapatty@gmail.com>
2019-06-01 02:22:47 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -1,10 +1,21 @@
 [{
-	"dia_s": "segunda",
-	"dia_m": "26",
-	"mes": "agosto",
-	"programacao": {
-		"titulo": "Credenciamento",
-		"horario": "8:30",
-		"local": "Saguão C3"
+		"dia_s": "segunda",
+		"dia_m": "26",
+		"mes": "agosto"
+	},
+	{
+		"dia_s": "terça",
+		"dia_m": "27",
+		"mes": "agosto"
+	},
+	{
+		"dia_s": "quarta",
+		"dia_m": "28",
+		"mes": "agosto"
+	},
+	{
+		"dia_s": "quinta",
+		"dia_m": "29",
+		"mes": "agosto"
 	}
-}]
+]
