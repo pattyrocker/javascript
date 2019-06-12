827a3bf5b24af6f6cb4434d2d3b7460275f7d909
pattyrocker <layoutsdapatty@gmail.com>
2019-06-11 23:01:53 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -2,48 +2,80 @@
 		"dia_s": "segunda",
 		"dia_m": "26",
 		"mes": "agosto",
-		"programacao": {
+		"programacao": [{
+				"titulo": "Palestra 1",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
 				"titulo": "Palestra 1",
 				"horario": "8:30",
 				"local": "Saguão C3",
 				"Palestrante": "Nome do Palestrante",
 				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
 			}
+		]
 	},
 	{
 		"dia_s": "terça",
 		"dia_m": "27",
 		"mes": "agosto",
-		"programacao": {
-			"titulo": "Credenciamento",
-			"horario": "8:30",
-			"local": "Saguão C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
+		"programacao": [{
+				"titulo": "Credenciamento",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Credenciamento 2",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			}
+		]
 	},
 	{
 		"dia_s": "quarta",
 		"dia_m": "28",
 		"mes": "agosto",
-		"programacao": {
-			"titulo": "Cerimônia de Abertura",
-			"horario": "8:30",
-			"local": "Auditório C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
+		"programacao": [{
+				"titulo": "Cerimonia de abertura",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Cerimonia de abertura 2",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			}
+		]
 	},
 	{
 		"dia_s": "quinta",
 		"dia_m": "29",
 		"mes": "agosto",
-		"programacao": {
-			"titulo": "Cerimônia de Abertura",
-			"horario": "8:30",
-			"local": "Auditório C3",
-			"Palestrante": "Nome do Palestrante",
-			"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
-		}
+		"programacao": [{
+				"titulo": "Cerimonia de abertura 3",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Cerimonia de abertura 4",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			}
+		]
 	}
 ]
