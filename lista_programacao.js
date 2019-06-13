c30fee82e625ad1323e4e7f43237c60293c067c9
pattyrocker <layoutsdapatty@gmail.com>
2019-06-12 21:16:48 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -77,5 +77,25 @@
 				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
 			}
 		]
+	},
+	{
+		"dia_s": "sexta",
+		"dia_m": "30",
+		"mes": "agosto",
+		"programacao": [{
+				"titulo": "Cerimonia de abertura 5",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Cerimonia de abertura 6",
+				"horario": "8:30",
+				"local": "Saguão C3",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			}
+		]
 	}
 ]
