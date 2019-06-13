47fa8f6984daa532751cc08010a9d172d336e396
pattyrocker <layoutsdapatty@gmail.com>
2019-06-13 00:53:56 -0300

    Update lista_programacao.js

--- lista_programacao.js
+++ lista_programacao.js
@@ -2,11 +2,66 @@
 		"dia_s": "segunda",
 		"dia_m": "26",
 		"mes": "agosto",
-		"programacao": [
-			{
+		"programacao": [{
 				"titulo": "Credenciamento",
 				"horario": "8:00",
 				"local": "Saguão C3",
+				"Palestrante": null,
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Cerimônia de Abertura",
+				"horario": "9:30",
+				"local": "Auditório C3",
+				"Palestrante": null,
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Pitch Hackathon",
+				"horario": "10:30",
+				"local": "Local",
+				"Palestrante": null,
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Pausa para Almoço",
+				"horario": "12:00",
+				"local": "Local",
+				"Palestrante": null,
+				"descricao": null
+			},
+			{
+				"titulo": "Qiron Robotis e o Beo: Criando um robô Humanóide do Zero",
+				"horario": "13:30",
+				"local": "Local",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Comportamentos Adaptivos em Robôs Evoluídos",
+				"horario": "14:00",
+				"local": "Local",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Impactos do IOT na Sociedade e no Mundo Adaptativo",
+				"horario": "14:30",
+				"local": "Local",
+				"Palestrante": "Nome do Palestrante",
+				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
+			},
+			{
+				"titulo": "Coffe Break",
+				"horario": "15:30",
+				"local": "Local",
+				"Palestrante": null,
+				"descricao": null
+			},
+			{
+				"titulo": "Mesa Projeto: Robótica e IOT",
+				"horario": "16:00",
+				"local": "Local",
 				"Palestrante": null,
 				"descricao": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque sed lectus quis porttitor. Nulla facilisis feugiat libero id accumsan. Nam imperdiet fringilla porttitor. In eget bibendum leo. Quisque quis."
 			}
